d3c66a43991f488cd9db897cfcbd17daf3de0f1a
katebriggs <katebriggs.dev@gmail.com>
2022-05-25 22:56:55 -0500

    Established player list render hook

--- scripts/module.js
+++ scripts/module.js
@@ -1,3 +1,29 @@
+
+class HarmTracker{
+
+    static HarmData(obj) {
+        if('data' in obj && 'harm' in obj.data){
+            return obj.data.harm;
+        }
+        return undefined;
+    }
+
+    static async onUpdateActor(doc,changes,options,userId){
+        if(HarmData(changes)){
+            console.log(HarmData(doc.data));
+        }
+    }
+    static async onRenderPlayerList(app,html,data){
+        for(var listItem of html.find("ol#player-list > li")){
+            var button = document.createElement("button");
+            button.style.flex = "flex";
+            button.innerHTML = "Pog";
+            listItem.append(button);            
+        }
+    }
+}
+
+
 Hooks.once('init', async function() {
     console.log("HELLO WORLD");
 });
@@ -6,8 +32,6 @@ Hooks.once('ready', async function() {
 
 });
 
-async function onUpdateActor(doc,changes,options,userId){
-    console.log(changes);
-}
 
-Hooks.on('updateActor', onUpdateActor);
+Hooks.on('updateActor', HarmTracker.onUpdateActor);
+Hooks.on('renderPlayerList', HarmTracker.onRenderPlayerList);
