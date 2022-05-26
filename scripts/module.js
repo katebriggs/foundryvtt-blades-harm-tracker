0fc6a3124b8d461b56a0d41d4574ec468d88b837
katebriggs <katebriggs.dev@gmail.com>
2022-05-25 20:52:28 -0500

    Added the basics

--- scripts/module.js
+++ scripts/module.js
@@ -5,3 +5,9 @@ Hooks.once('init', async function() {
 Hooks.once('ready', async function() {
 
 });
+
+async function onUpdateActor(doc,changes,options,userId){
+    console.log(changes);
+}
+
+Hooks.on('updateActor', onUpdateActor);
